e09ca9e3ec706d8ae8dfceaa0452fb82d12ad78c
Juan Gabriel Fraire López <groteck@gmail.com>
2014-04-30 02:15:02 +0100

    basic cookbook funcional project

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'rails-machine'
+maintainer       'YOUR_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures rails-machine'
+long_description 'Installs/Configures rails-machine'
+version          '0.1.0'
+
